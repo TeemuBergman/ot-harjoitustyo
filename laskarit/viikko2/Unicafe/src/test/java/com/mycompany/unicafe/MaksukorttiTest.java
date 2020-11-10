27b086e3121ba8991f2ac46c0c5800f163678073
Teemu Bergman <teemu.bergman@helsinki.fi>
2020-11-10 23:21:12 +0200

    Koodin rivien välit korjattu.

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,36 +15,34 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
-    
+
     @Test
     public void kortinSaldoOnOikein() {
         assertEquals(1000, kortti.saldo());
     }
-    
-    
+
     @Test
     public void saldoKasvaaOikein() {
         kortti.lataaRahaa(1000);
         assertEquals(2000, kortti.saldo());
     }
-    
+
     @Test
     public void saldoEiMuutuJosSitaEiOleTarpeeksi() {
         kortti.otaRahaa(2000);
         assertEquals(1000, kortti.saldo());
     }
-    
+
     @Test
     public void trueJosSaldoRiitti() {
         assertTrue(kortti.otaRahaa(1000));
     }
-    
+
     @Test
     public void falseJosSaldoEiRiittanyt() {
         assertFalse(kortti.otaRahaa(2000));
     }
-    
-    
+
 }
