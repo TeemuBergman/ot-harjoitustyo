0c3266e0c1aca7c125c0fd9ed28fb34ed6d943eb
Teemu Bergman <teemu.bergman@helsinki.fi>
2020-12-02 07:24:49 +0200

    Added "cut card" random placement functionality

--- Blackjack/src/shoe.py
+++ Blackjack/src/shoe.py
@@ -1,4 +1,5 @@
-""""In Blackjack shoe is a combination of 6 card decks of 52 cards, total of 312 cards."""
+""""In Blackjack shoe is a combination of 6 card decks of 52 cards, total of 312 cards.
+    Point where shoe will be resuffled is marked by the cut card."""
 
 import random
 
@@ -8,19 +9,21 @@ class Shoe:
 
     def __init__(self):
         self.shoe = []
+        self.cut_card = 0
         self.create_shoe()
 
     def create_shoe(self):
-        """This is where Shoe is created."""
+        """Create a Shoe and place a "cut card" somewhere in the last quarter of Shoe."""
         for _ in range(6):
             for _ in range(4):
                 for card in range(13):
                     self.shoe.append(card + 1)
         random.shuffle(self.shoe)
+        self.cut_card = random.randrange(0, 78)
 
     def get_card(self):
         """Get one card from Shoe."""
-        if len(self.shoe) == 0:
+        if len(self.shoe) == self.cut_card:
             print("SHUFFLING DECK")
             self.create_shoe()
         card = self.shoe.pop()
@@ -29,5 +32,5 @@ class Shoe:
         return card
 
     def get_size(self):
-        """Return how many cards left in Shoe."""
+        """Return how many cards are left in Shoe."""
         return len(self.shoe)
