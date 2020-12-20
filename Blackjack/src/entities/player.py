2d0acb4b081617754bc7084f5b29715be0c636f5
Teemu Bergman <teemu.bergman@helsinki.fi>
2020-12-20 23:00:20 +0200

    Refaktointia

--- Blackjack/src/entities/player.py
+++ Blackjack/src/entities/player.py
@@ -16,11 +16,20 @@ class Player:
         self.hand = Hand()
 
     def cash_add(self, amount):
-        """ Add cash to Players stack. """
+        """Add cash to Players stack. """
         self.cash_balance += amount
         return self.cash_balance
 
     def cash_reduce(self, bet):
-        """ Reduce Players stack of cash by the amount of current bet. """
+        """Reduce Players stack of cash by the amount of current bet. """
         self.cash_balance -= bet
         return self.cash_balance
+
+    def save_game(self):
+        """Return Player data as tuple."""
+        return self.name, self.cash_balance
+
+    def load_game(self, data):
+        """Load saved game data as a tuple and replace current data."""
+        self.name = data[0]
+        self.cash_balance = int(data[1])
